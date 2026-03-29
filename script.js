ee631d06ba5bd7fe37a57dc64bd1c5615303d1df
SitesVitor <vitoralexandreguimaraes09@gmail.com>
2026-03-29 11:21:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const fechar = document.getElementById("fechar");
 
 const card1 = document.getElementById("Cards");
 const card2 = document.getElementById("Cards1");
+const card3 = document.getElementById("Cards2");
 
 // ABRIR
 abrir.addEventListener("click", () => {
@@ -26,19 +27,37 @@ abrir.addEventListener("click", () => {
 
     }, 300);
 
+    setTimeout(() => {
+        card3.style.display = "block";
+        card3.classList.remove("esconder");
+
+        setTimeout(() => {
+            card3.classList.add("mostrar");
+        }, 50);
+    }, 500);
+    
     fechar.style.display = "block";
 });
 
 // FECHAR
 fechar.addEventListener("click", () => {
 
-    // primeiro o segundo
-    card2.classList.remove("mostrar");
-    card2.classList.add("esconder");
+    card3.classList.remove("mostrar");
+    card3.classList.add("esconder");
 
     setTimeout(() => {
-        card2.style.display = "none";
+        card3.style.display = "none";
     }, 400);
+    
+    setTimeout(() => {
+        card2.classList.remove("mostrar");
+        card2.classList.add("esconder");
+
+        setTimeout(() => {
+            card2.style.display = "none";
+        }, 400);
+
+    }, 200);
 
     // depois o primeiro
     setTimeout(() => {
